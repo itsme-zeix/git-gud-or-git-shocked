8af36ad3b0d5c62b3048323202dc9a509bcf595c
leyew <102467346+itsme-zeix@users.noreply.github.com>
2025-01-18 21:17:45 +0800

    Fix style

--- valo-tracking/region_capture.py
+++ valo-tracking/region_capture.py
@@ -6,58 +6,52 @@ import time
 
 class RegionCapture:
     def __init__(self):
-        self.HEALTH_BAR_HORIZONTAL_COORDS_PERCENTAGE = 0.3 #percentage of width at which the health will appear
+        # Percentage of width at which the health will appear
+        self.HEALTH_BAR_HORIZONTAL_COORDS_PERCENTAGE = 0.3 
         self.HEALTH_BAR_VERTICAL_COORDS_PERCENTAGE = 0.85
         self.HEALTH_BAR_WIDTH_PERCENTAGE = 0.1
         self.image_count = 0
 
     def detect_screen_changes(self):
-        """
-        Captures frames continuously
-        """
         while True:
             with mss.mss() as sct:
-                    # Define the screen region (full screen if None)
-                    monitor = sct.monitors[0]
-                    region = self._get_bottom_30_percent(monitor_dimensions=monitor)
-                    # Initialize variables
-                    last_health_value = None # format of health
-                    print("Monitoring screen for changes... Press 'Ctrl+C' to stop.")
-                    try:
-                        # Capture the current frame
-                        current_frame = np.array(sct.grab(region))
-                        # Convert BGRA to grayscale for comparison
-                        gray_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGRA2GRAY)
-                        current_health_value = ocr_reader.read_image(gray_frame)
-                        print(f"captured current health value: {current_health_value}")
-                        if current_health_value is not None:
-                            is_current_health_value_numeric = str.isnumeric(current_health_value)
-                            self._save_frame(gray_frame, current_health_value)
+                # Define the screen region (full screen if None)
+                monitor = sct.monitors[0]
+                region = self._get_bottom_30_percent(monitor_dimensions=monitor)
+                # Initialize variables
+                last_health_value = None # format of health
+                print("Monitoring screen for changes... Press 'Ctrl+C' to stop.")
+                try:
+                    # Capture the current frame
+                    current_frame = np.array(sct.grab(region))
+
+                    # Convert BGRA to grayscale for comparison
+                    gray_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGRA2GRAY)
+                    current_health_value = ocr_reader.read_image(gray_frame)
+                    print(f"Captured current health value: {current_health_value}")
+                    
+                    # Filter for numbers
+                    if current_health_value is not None:
+                        is_current_health_value_numeric = str.isnumeric(current_health_value)
+
+                        self._save_frame(gray_frame, current_health_value)
                         # Display the current frame (optional)
                         # cv2.imshow("Current Frame", gray_frame)
-                            if is_current_health_value_numeric:
-                                if (self._has_health_drop(current_health_value, last_health_value)):
-                                    self._send_electrical_shock_signal()
-                                time.sleep(2)
-                            elif is_current_health_value_numeric:
-                                last_health_value = current_health_value
-                        # Compare with the previous frame if it exists
-                        # if last_health_value is not None:
-                        #     # Calculate the absolute difference between the health shown in the two frames
-                        #     if (self._has_health_drop(current_health_value, last_health_value)):
-                        #         self.send_electrical_shock_signal()
-                        #         time.sleep(0.5)
-                        # last_health_value = current_health_value
-                        # Exit on pressing ESC
-                        if cv2.waitKey(1) & 0xFF == ord("\\"):
-                            break
 
-                    except KeyboardInterrupt:
-                        print("\nMonitoring stopped by user.")
+                        if is_current_health_value_numeric:
+                            if (self._has_health_drop(current_health_value, last_health_value)):
+                                self._send_electrical_shock_signal()
+                            time.sleep(2)
+                        elif is_current_health_value_numeric:
+                            last_health_value = current_health_value
+                    if cv2.waitKey(1) & 0xFF == ord("\\"):
                         break
 
-                    finally:
-                        cv2.destroyAllWindows()\
+                except KeyboardInterrupt:
+                    print("\nMonitoring stopped by user.")
+                    break
+                finally:
+                    cv2.destroyAllWindows()
 
     def _has_health_drop(self, current_value, last_value):
         return last_value - current_value > 0
